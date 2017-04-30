153ca2da031ffe08e4f2a6780ffae963eb6b4db7
Apichart Homkeaw <angelerkizer.top@gmail.com>
2017-05-01 01:19:49 +0700

    Format code

--- Usedcar/src/main/java/com/oot/usedcar/service/InitialDataService.java
+++ Usedcar/src/main/java/com/oot/usedcar/service/InitialDataService.java
@@ -30,38 +30,39 @@ import com.oot.usedcar.service.user.UserService;;
 
 @Service
 public class InitialDataService {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private CarRepository carRepository;
-	
+
 	@Autowired
 	private ProvinceRepository provinceRepository;
-	  
+
 	@Autowired
 	private BuyCarRepository buyCarRepository;
-	
+
 	@Autowired
 	private UsedCarRepository usedCarRepository;
-	
+
 	private List<String> fileList = null;
-	public void initailUser(){
-		User user = new User(); 
-    	user.setUsername("test");
-    	user.setPassword("test");
-    	userService.save(user);
+
+	public void initailUser() {
+		User user = new User();
+		user.setUsername("test");
+		user.setPassword("test");
+		userService.save(user);
 	}
-	
-	public void initailCar(){
+
+	public void initailCar() {
 		this.fileList = new ArrayList<>();
 		String sourceFolder = Paths.get(".").toAbsolutePath().normalize().toString() + "/CAR";
 		List<String> fileList = generateFileList(new File(sourceFolder), sourceFolder);
 		fileList.remove(0);
-        for (String file : fileList) {
-			String filename = sourceFolder+file;
-			try {		
+		for (String file : fileList) {
+			String filename = sourceFolder + file;
+			try {
 				Gson gson = new Gson();
 				SCBCar scbCarToyota = gson.fromJson(new FileReader(filename), SCBCar.class);
 				List<SCBCarData> scbCarDataListToyota = scbCarToyota.getData();
@@ -77,11 +78,11 @@ public class InitialDataService {
 			} catch (JsonSyntaxException | JsonIOException | FileNotFoundException e) {
 				e.printStackTrace();
 			}
-				
+
 		}
 	}
-	
-	public void initailUsedCar(){
+
+	public void initailUsedCar() {
 		UsedCar used_car = new UsedCar();
 		used_car.setBrand("HONDA");
 		used_car.setModel("Jazz");
@@ -93,7 +94,7 @@ public class InitialDataService {
 		used_car.setCarId("กข-1234");
 		used_car.setStatus("Available");
 		usedCarRepository.save(used_car);
-		
+
 		UsedCar used_car1 = new UsedCar();
 		used_car1.setBrand("HONDA");
 		used_car1.setModel("Civic");
@@ -105,7 +106,7 @@ public class InitialDataService {
 		used_car1.setCarId("ภม-5678");
 		used_car1.setStatus("Available");
 		usedCarRepository.save(used_car1);
-		
+
 		UsedCar used_car2 = new UsedCar();
 		used_car2.setBrand("HONDA");
 		used_car2.setModel("Accord");
@@ -118,13 +119,13 @@ public class InitialDataService {
 		used_car2.setStatus("Available");
 		usedCarRepository.save(used_car2);
 	}
-	
+
 	public void initailProvince() {
 		Province prov = new Province();
 		prov.setNameth("กรุงเทพมหานคร");
 		prov.setNameen("Bangkok");
 		provinceRepository.save(prov);
-		
+
 		prov = new Province();
 		prov.setNameth("สมุทรปราการ");
 		prov.setNameen("Samut Prakan");
@@ -136,69 +137,48 @@ public class InitialDataService {
 		provinceRepository.save(prov);
 
 	}
-		 
-	
-	public void initailBuyCar(){
+
+	public void initailBuyCar() {
 		BuyCar buycar = new BuyCar();
 		buycar.setGender("M");
 		buycar.setName("KITTI");
 		buycar.setLastname("Hongsa");
-//		buycar.setBuyDate("");
-//		buycar.setBuyPrice(300000);
+		// buycar.setBuyDate("");
+		// buycar.setBuyPrice(300000);
 		buyCarRepository.save(buycar);
-		
-		
+
 	}
-	
-	public List<Car> getCarList(){
-		List<Car> carList = new ArrayList();
-		Car car = new Car();
-		car.setBrand("TOYOTA");
-		car.setModel("Altis");
-		car.setYear(2015);
-		car.setMiddlePrice(new BigDecimal("500000.00"));
-		carList.add(car);
-		
-		Car car2 = new Car();
-		car2.setBrand("HONDA");
-		car2.setModel("CIVIC");
-		car2.setYear(2015);
-		car2.setMiddlePrice(new BigDecimal("500000.00"));
-		carList.add(car2);
-		
-		return carList;
-	}
-	
-	public List<PaymentMethod> getPaymentMethodList(){
+
+	public List<PaymentMethod> getPaymentMethodList() {
 		List<PaymentMethod> payList = new ArrayList<>();
-		
+
 		PaymentMethod pay1 = new PaymentMethod();
 		pay1.setPayKey("C");
 		pay1.setPayValue("Cash");
 		payList.add(pay1);
-		
+
 		PaymentMethod pay2 = new PaymentMethod();
 		pay2.setPayKey("F");
 		pay2.setPayValue("Finance");
 		payList.add(pay2);
-		
+
 		return payList;
 	}
-	
+
 	private List<String> generateFileList(File node, String sourceFolder) {
-        if (node.isFile()) {
-        	this.fileList.add(generateFilePath(node.getAbsoluteFile().toString(), sourceFolder));
-        }
-        if (node.isDirectory()) {
-            String[] subNote = node.list();
-            for (String filename : subNote) {
-                generateFileList(new File(node, filename), sourceFolder);
-            }
-        }
-        return this.fileList;
-    }
-	
+		if (node.isFile()) {
+			this.fileList.add(generateFilePath(node.getAbsoluteFile().toString(), sourceFolder));
+		}
+		if (node.isDirectory()) {
+			String[] subNote = node.list();
+			for (String filename : subNote) {
+				generateFileList(new File(node, filename), sourceFolder);
+			}
+		}
+		return this.fileList;
+	}
+
 	private String generateFilePath(String file, String sourceFolder) {
-        return file.substring(sourceFolder.length(), file.length());
-    }
+		return file.substring(sourceFolder.length(), file.length());
+	}
 }
